97e59cbf457fde3d4cb50d10b8d39a3df4a09442
lastlegend1234 <deshmukhvinayak1234@gmail.com>
2025-04-05 12:00:17 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import {useState,useRef} from 'react'
+import './App.css';
+
+function App() {
+  const [currentCount,setCurrentCount] = useState(0);
+  const timer = useRef();
+
+  function startClock(){
+    let value = setInterval(function(){
+      setCurrentCount(c => c+1)
+    },1000);
+    timer.current =value;
+  }
+
+  function stopClock(){
+    clearInterval(timer.current);
+  }
+
+  return <div>
+    {currentCount}
+    <br></br>
+    <button onClick={startClock}> Start</button>
+    <button onClick ={stopClock}>Stop</button>
+  </div>
+}
+
+export default App;
